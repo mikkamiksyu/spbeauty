d65f858ecbba7d8fa0245324139d233b43954ca9
mikkamiksyu <137329965+mikkamiksyu@users.noreply.github.com>
2023-07-30 21:55:49 +0000

    выполнены правки по заданию

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,16 +1,28 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+import styled from "styled-components";
+
+const Option = styled.div`
+border:3px solid #F8FAFC;
+border-radius: 8px;
+padding: 24px
+`;
+
+const Option2 = styled.div`
+padding: 24px 0px 24px 0px
+`;
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
+        <Option className='d-inline-flex flex-column gap-2 mt-4'>
             <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+            <Option2 className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
-        </div>
+                <SecondaryButton>Добавить филиал</SecondaryButton>
+            </Option2>
+        </Option>
     );
 }
 
--- src/Layouts/Page/AboutCompany/Components/Heading.js
+++ src/Layouts/Page/AboutCompany/Components/Heading.js
@@ -1,10 +1,19 @@
 import PrimaryButton from "../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../Components/SecondaryButton";
+import styled from 'styled-components';
+const Option = styled.div`
+`;
+/*outline: none;
+border-radius: 24px;
+border: solid #E2E8F0 1px;
+background-color: #F8FAFC;
+color: #0F172A;*/
+
 function Heading() {
     return (
         <div className='d-flex flex-row justify-content-between mt-5'>
             <h1 className='large-title'>Данные о компании</h1>
-            <PrimaryButton>Закрыть запись</PrimaryButton>
+            <SecondaryButton>Закрыть запись</SecondaryButton>
         </div>
     );
 }
